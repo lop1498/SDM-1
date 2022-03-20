44ccf96b2484585ba44e0dcc7e787e5f2f6f259a
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2022-03-20 15:43:07 +0100

    add authors de articles a papers

--- src/transform_data.py
+++ src/transform_data.py
@@ -2,6 +2,7 @@ from neo4j import GraphDatabase
 import numpy as np
 import pandas as pd
 import urllib.parse
+from sklearn.utils import shuffle
 
 
 class Neo4jConnection:
@@ -74,7 +75,7 @@ def add_authors(path, path_db):
     query2 = '''
             LOAD CSV WITH HEADERS FROM $p2 AS line2
             MATCH (a:Author {id: line2.id}), (art:Article {title: line2.title})
-            CREATE (a)-[r:writes]->(art)
+            CREATE (a)-[r:writes_article]->(art)
             '''
     conn.query(query1, parameters={'p1': p1})
     conn.query(query2, parameters={'p2': p2})
@@ -113,30 +114,30 @@ def add_articles(path, path_db):
 
     return
 
+
 def add_papers(path, path_db):
-    l = list(pd.read_csv(path+'dblp_phdthesis_header.csv', sep=';').columns)
+    l = list(pd.read_csv(path + 'dblp_phdthesis_header.csv', sep=';').columns)
     names = [name.split(':')[0] for name in l]
-    df = pd.read_csv(path+'dblp_phdthesis.csv', nrows=10000, sep=';', names=names)
+    df = pd.read_csv(path + 'dblp_phdthesis.csv', nrows=10000, sep=';', names=names)
     df = df[['phdthesis', 'author', 'title', 'mdate', 'key', 'year', 'author']].dropna()
 
     df.to_csv(path_db + "/papers.csv", index=False)
     p1 = "file:///papers.csv"
 
-
     query1 = '''
-                LOAD CSV WITH HEADERS FROM $p1 AS line1
-                CREATE(:Paper {key: line1.key, date: line1.mdate, title: line1.title})
-                '''
+                    LOAD CSV WITH HEADERS FROM $p1 AS line1
+                    CREATE(:Paper {key: line1.key, date: line1.mdate, title: line1.title})
+                    '''
 
     conn.query(query1, parameters={'p1': p1})
-    return
+
 
 def add_papers_authors(path, path_db):
     # llegir els papers
     l = list(pd.read_csv(path + 'dblp_phdthesis_header.csv', sep=';').columns)
     names = [name.split(':')[0] for name in l]
     papers = pd.read_csv(path + 'dblp_phdthesis.csv', nrows=10000, sep=';', names=names)
-    papers = papers[['phdthesis','volume', 'author', 'title', 'mdate', 'key', 'year']].dropna()
+    papers = papers[['phdthesis', 'volume', 'author', 'title', 'mdate', 'key', 'year']].dropna()
 
     papers['author'] = papers['author'].map(lambda x: list(x.split('|')))
     papers = papers.explode('author')
@@ -144,29 +145,52 @@ def add_papers_authors(path, path_db):
     papers.to_csv(path_db + "/papers_authors_edges.csv", index=False)
     p1 = "file:///papers_authors_edges.csv"
 
+    authors = pd.read_csv(path+'dblp_author.csv', header=[0], nrows=10000, sep=';')
+    authors.rename(columns={':ID':'id', 'author:string': 'author'}, inplace=True)
+
+    authors['_tmpkey'] = 1
+    papers['_tmpkey'] = 1
+    conc = pd.merge(authors, papers, on='_tmpkey').drop('_tmpkey', axis=1).sample(frac=0.0025)
+
+    conc.to_csv(path_db + "/papers_authors_more_edges.csv", index=False)
+    p2 = "file:///papers_authors_more_edges.csv"
+
     query1 = '''
-            LOAD CSV WITH HEADERS FROM $p1 AS line
-            CREATE(a:Author {id: line.phdthesis, name: line.author})
-            '''
+                LOAD CSV WITH HEADERS FROM $p1 AS line
+                MERGE(a:Author {id: line.phdthesis, name: line.author})
+                '''
 
     query2 = '''
-            LOAD CSV WITH HEADERS FROM $p1 AS line2
-            MATCH (a:Author {id: line2.phdthesis}), (pap:Paper {title: line2.title})
-            CREATE (a)-[r:writes]->(pap)
-            '''
+                LOAD CSV WITH HEADERS FROM $p1 AS line2
+                MATCH (a:Author {id: line2.phdthesis}), (pap:Paper {title: line2.title})
+                CREATE (a)-[r:writes_paper]->(pap)
+                CREATE (pap)-[c:corresponding_author]->(a)
+                '''
+
+    query3 = '''
+                LOAD CSV WITH HEADERS FROM $p2 AS line3
+                MERGE (a:Author {id: line3.id, name: line3.author_x})
+                WITH a, line3
+                MATCH (a:Author {id: line3.id}), (pap:Paper {key: line3.key})
+                MERGE (a)-[:writes_paper]->(pap)
+                '''
+
     conn.query(query1, parameters={'p1': p1})
     conn.query(query2, parameters={'p1': p1})
+    conn.query(query3, parameters={'p2': p2})
 
     return
 
-if __name__ == "__main__":
-    paths_file = open('src/paths.txt', 'r')
 
-    path = paths_file.readline()[:-1] # Porque lee el \n del salto de linea
-    path_db = paths_file.readline()
+if __name__ == "__main__":
+    #paths_file = open('src/paths.txt', 'r')
+    #path = paths_file.readline()[:-1] # Porque lee el \n del salto de linea
+    #path_db = paths_file.readline()
+    path = '/Users/lop1498/Desktop/MDS/Q2/SDM/lab1/data/'
+    path_db = '/Users/lop1498/Library/Application Support/Neo4j Desktop/Application/relate-data/dbmss/dbms-a925e2f5-b2ac-42e3-b89e-1ea1b962a96b/import'
 
     clean_database()
-    add_papers(path, path_db)
-    add_papers_authors(path, path_db)
     add_articles(path, path_db)
     add_authors(path, path_db)
+    add_papers(path, path_db)
+    add_papers_authors(path, path_db)
